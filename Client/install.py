0c7b4437050aaa77dbcb83b633317bed607870c4
zhengyulennyli <zhengyu.lenny.li@gmail.com>
2013-10-27 00:14:05 -0400

    user account library and machine library code. beginning of install code

--- Client/install.py
+++ Client/install.py
@@ -21,3 +21,34 @@ def install():
         username, hashed_password = create_account()
 
     machine_name = get_machine_name()
+    folder_path = get_folder_path()
+    if(store_new_user_information(username, hashed_password, machine_name, folder_path)):
+        print "Installation Success!"
+        return True
+    else:
+        print "Communication with service failed, try again later"
+        return False
+
+def get_folder_path():
+    valid_path = False
+    while(not valid_path):
+        path = raw_input("Enter exact path:")
+        if(path_is_valid(path)):
+            valid_path = True
+        else:
+            print "Invalid path."
+
+def store_new_user_information(username, hashed_password, machine_name, folder_path):
+    #todo: talk to server and try to store information
+    return False
+
+def path_exists(path):
+    #todo: check if path exists
+    return False
+
+def path_is_valid(path):
+    #todo: check if path can be created
+    return True
+
+if __name__ == "__main__":
+    main()
